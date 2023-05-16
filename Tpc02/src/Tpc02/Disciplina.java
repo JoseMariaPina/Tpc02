611c203ad41c4d977de9a8bcc4a6fd03ed270185
JoseMariaPina <133685825+JoseMariaPina@users.noreply.github.com>
2023-05-16 16:04:39 -0100

    Add files via upload

---
+++ Tpc02/src/Tpc02/Disciplina.java
@@ -0,0 +1,32 @@
+package Tpc02;
+
+public class Disciplina {
+	    private String titulo;
+	    private String docente;
+	    
+
+	    public Disciplina(String titulo, String docente) {
+	        this.titulo = titulo;
+	        this.docente = docente;
+	      
+	    }
+
+	    public String getTitulo() {
+	        return titulo;
+	    }
+
+	    public void setTitulo(String titulo) {
+	        this.titulo = titulo;
+	    }
+
+	    public String getDocente() {
+	        return docente;
+	    }
+
+	    public void setNomeDocente(String docente) {
+	        this.docente = docente;
+	    }
+
+	}
+
+
